dffd7f827f3fe99d3ff40bbcb1e4b3bbabdcc3c5
MrGaggio <steruggiu@gmail.com>
2021-10-14 14:20:14 +0100

    creato messaggio e Array con lista email

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,24 @@
 /* alert("prova!"); */
 
 
+//Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull'esito del controllo.
+
+/* STRUMENTI:
+
+- Prompt
+- Array
+- if / else
+
+
+*/
+
+
+// Chiedi all'utente la sua email
+
+const email = prompt('Inserisci la tua email');
+
+
+// Crea una lista di email
+
+const authorizedEmail = ["steruggiu@gmail.com", "mario.rossi@tiscali.it", "paperino@disney.com", "batman@dc.com"]
+console.log(authorizedEmail);
