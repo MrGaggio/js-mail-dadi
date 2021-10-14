c5511c26be26dedb042f8dee905f49b2452e26ed
MrGaggio <steruggiu@gmail.com>
2021-10-14 16:39:23 +0100

    terminato primo esercizio

--- assets/main.js
+++ assets/main.js
@@ -18,25 +18,30 @@
 
 const email = prompt('Inserisci la tua email');
 
-
 // Crea una lista di email
 
 const authorizedEmail = ["steruggiu@gmail.com", "mario.rossi@tiscali.it", "paperino@disney.com", "batman@dc.com"]
 
-
 // Se la mail è contenuta all'interno dell'array è autorizzata
 // Se la mail non è contenuta nell'array non è autorizzata
 
-
+let email_trovata = false
 
 for (let i = 0; i < authorizedEmail.length; i++) {
     const accesso = authorizedEmail[i];
     if (email == accesso) {
-    console.log('puoi continuare a inserire i tuoi dati')
-    }else if(email != accesso)
-    alert('mi spiace non sei autorizzato')
-    
+        email_trovata = true    
     }
+}
+ 
+console.log(email_trovata) ;
+
+if(email_trovata == true){
+    console.log('la tua email è verificata');
+}else if(email_trovata == false){
+    console.log('la tua mail non è verificata');
+}
+
 
 
 
@@ -45,7 +50,14 @@ for (let i = 0; i < authorizedEmail.length; i++) {
 // Genera un numero random da 1 a 6 sia per il giocatore che per il computer. Stabilire il vincitore in base a chi fa il punteggio + alto
 
 
-//Genera un numero random
+/* STRUMENTI    
 
+- Math.random
+- math
+
+
+
+
+*/
 
 
