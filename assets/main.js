eff39d04a1ae13382f32f15b899b7fcc54d3e9f0
MrGaggio <steruggiu@gmail.com>
2021-10-14 15:14:57 +0100

    avanzamento

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,7 @@
 
 - Prompt
 - Array
+- For
 - if / else
 
 
@@ -21,4 +22,22 @@ const email = prompt('Inserisci la tua email');
 // Crea una lista di email
 
 const authorizedEmail = ["steruggiu@gmail.com", "mario.rossi@tiscali.it", "paperino@disney.com", "batman@dc.com"]
-console.log(authorizedEmail);
+
+
+// Se la mail è contenuta all'interno dell'array è autorizzata
+// Se la mail non è contenuta nell'array non è autorizzata
+
+
+
+for (let i = 0; i < authorizedEmail.length; i++) {
+    const element = authorizedEmail[i];
+    console.log(element);
+    if (element == authorizedEmail) {
+    console.log('puoi continuare a inserire i tuoi dati')
+    }else (element != authorizedEmail)
+    console.log('mi spiace non sei autorizzato')
+    
+    }
+
+
+
