dfe7fe5ff09e763bd636ae95f1d9b895ffd6591a
MrGaggio <steruggiu@gmail.com>
2021-10-14 21:50:49 +0100

    prove esercizio dadi

--- assets/main_dadi.js
+++ assets/main_dadi.js
@@ -18,15 +18,34 @@
 
 // genera un numero random tra 1 e 6
 
-num = Math.round(Math.random() * 5) + 1;
+
+/* prompt('Premi invio per generare il tiro del') + " giocatore"; */
+function giocatore() {
+numGiocatore = Math.round(Math.random() * 5) + 1;
+console.log(numGiocatore);
+document.getElementById("giocatore").innerHTML ="Giocatore, il tuo tiro è pari a: " + num;
+}
+
+/* prompt('Adesso premi invio per generare la giocata del') + " computer" */
+function computer() {
+numComputer = Math.round(Math.random() * 5) + 1;
+document.getElementById("computer").innerHTML ="Computer, il tuo tiro è pari a: " + num;
+}
+
+if( numGiocatore > numComputer){
+    alert('mi dispiace computer, ha vinto il giocatore')
+}else if( numComputer > numGiocatore){
+    alert('mi dispiace giocatre, ha vino il computer')
+}else if( numGiocatore == numComputer){
+    alert('avete pareggiato, provate un nuovo tiro di dadi')
+}
+
+
+
+// fare 2 tiri di dadi, dare un nome a ogni tiro, stabilire il numero + alto tra i 2
 
 
 
 
 
-
-// assegnare giocatore e computer
-
-const x = "giocatore"
-const y = "computer"
 
